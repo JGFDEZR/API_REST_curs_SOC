6baa4abd785cbdbd852dcd32f33e96c7d1e3a0e5
José <jgfernandezramos@gmail.com>
2023-04-18 12:55:31 +0200

    delete i update

--- flask_todo_list/server/src/app_todo/persistencia_tasca_sqlite.py
+++ flask_todo_list/server/src/app_todo/persistencia_tasca_sqlite.py
@@ -8,9 +8,12 @@ import tasca
 class Persistencia_tasca_sqlite():
     def __init__(self, ruta):
         self._ruta = ruta
-        self._conn = sqlite3.connect(self._ruta)
+        if not self.existeixen_taules():
+            self.reset_database()
+        
 
     def desa(self, tasca):  # GUARDAR UN REGISTRO
+        self._conn = sqlite3.connect(self._ruta)
         titol = tasca.titol
         done  = tasca.done
         resultat = None
@@ -28,9 +31,11 @@ class Persistencia_tasca_sqlite():
             print("[X] IntegrityError: Aquesta tasca ja està registrada.")
         self._conn.commit()  #commit guarda los cambios en base datos
         cursor.close()
+        self._conn.close()
         return resultat
     
     def get_list(self):
+        self._conn = sqlite3.connect(self._ruta)
         consulta = "SELECT titol, done, rowid FROM tasques;"
         cursor = self._conn.cursor()
         cursor.execute(consulta)
@@ -39,18 +44,92 @@ class Persistencia_tasca_sqlite():
         for registre in llista:
             tarea = tasca.Tasca(self, registre[0], registre[1], registre[2])
             resultat.append(tarea)
+        cursor.close()
+        self._conn.close()
         return resultat
+    
+    def modifica_tasca(self, tasca):
+        resultat = None
+        self._conn = sqlite3.connect(self._ruta)
+        titol = tasca.titol
+        done  = tasca.done
+        id = tasca.id
+        consulta = f"update tasques set done={done}, titol='{titol}' where rowid={id};"
+        cursor = self._conn.cursor()
+        try:
+            cursor.execute(consulta)
+            resultat = tasca
+        except sqlite3.IntegrityError:
+            print("[X] IntegrityError: Aquest titol ja existeix.")
+        self._conn.commit()
+        cursor.close()
+        self._conn.close()
+        return resultat
+    
+    def esborra_tasca(self, id):
+        self._conn = sqlite3.connect(self._ruta)
+        consulta = f"delete from tasques where rowid= {id};"
+        cursor = self._conn.cursor()
+        cursor.execute(consulta)
+        self._conn.commit()
+        cursor.close()
+        self._conn.close()
+
+
+    def existeixen_taules(self):
+        self._conn = sqlite3.connect(self._ruta)
+        consulta = "SELECT * FROM tasques LIMIT 1;"
+        cursor = self._conn.cursor()
+        try:
+            cursor.execute(consulta)
+        except sqlite3.OperationalError:
+            cursor.close()
+            self._conn.close()
+            return False
+        cursor.close()
+        self._conn.close()
+        return True
+    
+    def reset_database(self):
+        self._conn = sqlite3.connect(self._ruta)
+        cursor = self._conn.cursor()  # cursor, necesario para añadir, etc...
+        consulta = "DROP TABLE if exists tasques;"
+        cursor = cursor.execute(consulta)
+        consulta = "CREATE TABLE if not exists tasques(titol TEXT UNIQUE, done BOOLEAN);"
+        cursor = cursor.execute(consulta)
+        self._conn.commit()  #commit guarda los cambios en base datos
+        cursor.close()
+        self._conn.close()
+      
+
+           
     
 
 
 def main():
+    id = None
+    titol = "Fer la bugada"
     persistencia = Persistencia_tasca_sqlite("deleteme.bd")
-    una_tasca = tasca.Tasca(persistencia, "Fregar la escalera")
+    una_tasca = tasca.Tasca(persistencia, titol)
     print(persistencia.desa(una_tasca))
     tasques = persistencia.get_list()
     print("-----Llista de tasques----")
     for taska in tasques:
         print(taska)
+        if taska.titol == titol:
+            id = taska.id
+            taska.titol = "Refer la bugada"
+            taska.done = True
+            persistencia.modifica_tasca(taska)
+            print("-------Taska modificada----")
+    tasques = persistencia.get_list()
+    print("\n\n--- Llista de tasques: ---")
+    for taska in tasques:
+        print(taska)
+    if id:
+        persistencia.esborra_tasca(id)
+        print("---Taska esborrada---")
+
 
 
 if __name__=="__main__":
--- flask_todo_list/server/src/app_todo/prova_app_tasques.py
+++ flask_todo_list/server/src/app_todo/prova_app_tasques.py
@@ -4,6 +4,7 @@ import app_tasques
 import tasca
 
 def main():
+    t = None
     la_app = app_tasques.App_tasques()
 
     la_app.afegeix_tasca(tasca.Tasca(None, "Escombrar las escales")) # creamos nueva tasca
@@ -11,6 +12,15 @@ def main():
 
     for t in la_app.llegir_tasques(): # bucle para leer las tascas que hemos hecho
         print(t)
+
+    if t:
+        t.done = True
+        la_app.modifica_tasca(t)
+        print("Tasca modificada")
+
+    if t:
+        la_app.esborra_tasca(t.id)
+        print("Tasca esborrada")
 
 if __name__ == "__main__":
     main()
